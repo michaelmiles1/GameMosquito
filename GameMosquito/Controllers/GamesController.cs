010565ab1df9d7fe26f112da66c555bf261d6d7c
michaelmiles1 <michael.gage.miles@gmail.com>
2019-10-01 16:14:35 -0700

    Initial Commit

---
+++ GameMosquito/Controllers/GamesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GameMosquito.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GameMosquito.Controllers
+{
+    public class GamesController : Controller
+    {
+        // GET: Games/Random
+        public IActionResult Random()
+        {
+            var game = new Game() {Name = "Grand Theft Auto 5"};
+
+            return View(game);
+        }
+    }
+}

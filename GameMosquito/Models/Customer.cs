480c9b6fa412cca98db7109233eb07cce707ec2f
michaelmiles1 <michael.gage.miles@gmail.com>
2019-10-01 19:14:37 -0700

    Finished video tutorial.

--- GameMosquito/Models/Customer.cs
+++ GameMosquito/Models/Customer.cs
@@ -3,7 +3,7 @@ namespace GameMosquito.Models
 {
     public class Customer
     {
-        public int Id { get; set; };
-        public string Name { get; set; };
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
